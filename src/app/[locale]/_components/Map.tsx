5b130bf163b381dcf1a53499c0048a67a171f67a
Elia Ferraro <ferraro.eli.99@gmail.com>
2026-06-29 23:12:58 +0200

    feat: Update map iframe source to show it in satellite mode

--- src/app/[locale]/_components/Map.tsx
+++ src/app/[locale]/_components/Map.tsx
@@ -6,7 +6,7 @@ export function Map() {
     <section id="mappa-section" className="w-full bg-[#F2EFE9] pt-12">
       <div className="w-full h-112.5 relative shadow-inner overflow-hidden">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2817.348633800613!2d8.3228965!3d44.9772095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4787948ef5336e1b%3A0x600ba4974f88e404!2sVia%20della%20Pace%2C%2024%2C%2014030%20Montemagno%20AT!5e0!3m2!1sit!2sit!4v1716750000000!5m2!1sit!2sit"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2378.3064615128733!2d8.32918422280645!3d44.97954837222017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47879796d9f3d325%3A0xb81fd54187ec5bf1!2sB%26B%20Il%20respiro%20del%20borgo%2C%20CODICE%20CIN%20IT005077C1SCRVTNU4!5e1!3m2!1sit!2sit!4v1782767467060!5m2!1sit!2sit"
           className="w-full h-full border-0 grayscale opacity-85 hover:grayscale-0 transition-all duration-700 ease-in-out"
           allowFullScreen={false}
           loading="lazy"
